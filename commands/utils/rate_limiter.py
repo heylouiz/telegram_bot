1f7a93fa4db501b1cbd42ec1abfcda4d0fce66bc
AtilioA <atiliogpg@protonmail.com>
2021-10-24 11:55:45 -0300

    Tweak rate limiter variable names

--- commands/utils/rate_limiter.py
+++ commands/utils/rate_limiter.py
@@ -9,10 +9,10 @@ rate_limit_logger = logging.getLogger(__name__)
 rate_limit_logger.setLevel(logging.DEBUG)
 
 
-GCP_TTS_CHAR_LIMIT = math.floor(1000000 / 31 / 24) - 1  # Max chars per hour
-RATE_LIMIT = 60 * 60  # 1 hour
+GCP_TTS_FREE_QUOTA = 1000000
+GCP_TTS_CHAR_LIMIT = math.floor(GCP_TTS_FREE_QUOTA / 31 / 24) - 1  # Max chars per hour
+RATE_LIMIT_TIMEFRAME = 60 * 60  # 1 hour
 used_chars = 0
-
 
 def rate_limited(max):
     """
@@ -35,16 +35,16 @@ def rate_limited(max):
 
             global used_chars
 
-            rate_limit_logger.warning(f"{used_chars}, {round(elapsed_first, 1)}, {round(elapsed_last, 1)}, {round(time.perf_counter(), 1)}, {round(last_time_called[0], 1)}")
+            rate_limit_logger.debug(f"{used_chars}, {round(elapsed_first, 1)}, {round(elapsed_last, 1)}, {round(time.perf_counter(), 1)}, {round(last_time_called[0], 1)}")
 
             # If char limit was exceeded within the time limit, stop
-            if used_chars > GCP_TTS_CHAR_LIMIT and elapsed_first < RATE_LIMIT:
-                rate_limit_logger.warning(f'Char limit exceeded within the time limit (used: {used_chars} under {round(elapsed_first, 2)} seconds, max: {GCP_TTS_CHAR_LIMIT} under {RATE_LIMIT} seconds).')
+            if used_chars > GCP_TTS_CHAR_LIMIT and elapsed_first < RATE_LIMIT_TIMEFRAME:
+                rate_limit_logger.warning(f'Char limit exceeded within the time limit (used: {used_chars} under {round(elapsed_first, 2)} seconds, max: {GCP_TTS_CHAR_LIMIT} under {RATE_LIMIT_TIMEFRAME} seconds).')
                 raise ValueError(f"Char limit exceeded within the time limit.")
             else:
-                if elapsed_first >= RATE_LIMIT:  # The time limit has expired,
+                if elapsed_first >= RATE_LIMIT_TIMEFRAME:  # The time limit has expired,
                     # Reset counters
-                    rate_limit_logger.debug(f'More than {RATE_LIMIT} seconds have passed, resetting rate limit counters...')
+                    rate_limit_logger.debug(f'More than {RATE_LIMIT_TIMEFRAME} seconds have passed, resetting rate limit counters...')
                     first_time_called[0] = time.perf_counter()
                     used_chars = 0
 
@@ -68,17 +68,18 @@ def rate_limited(max):
     return decorate
 
 
-@rate_limited(max=10)
-def print_num(num):
-    time.sleep(0.1)
-    pass
+if __name__ == '__main__':
+    @rate_limited(max=10)
+    def print_num(num):
+        time.sleep(0.1)
+        pass
 
 
-print("send print requests to decorated function")
-for i in range(1, 100):
-    try:
-        print_num(i)
-    except ValueError as e:
-        print(e)
-    else:
-        used_chars += i
+    print("Sending print requests to decorated function")
+    for i in range(1, 60):
+        try:
+            print_num(i)
+        except ValueError as e:
+            print(e)
+        else:
+            used_chars += i
